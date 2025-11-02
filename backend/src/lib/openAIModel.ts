7a2b619e247a5d78e72ea970cb1a8d56ac8c99ad
Muhammad Ahsan <imahsan600@gmail.com>
2025-11-02 21:58:12 +0500

    fix env's

--- backend/src/lib/openAIModel.ts
+++ backend/src/lib/openAIModel.ts
@@ -2,5 +2,4 @@ import OpenAI from 'openai'
 
 export const openai = new OpenAI({
 	apiKey: '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',
-	project: 'proj_Hwsh8jAwc2Qt92XDWNLw9Q7i',
 })
