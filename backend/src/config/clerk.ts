2caabd1bb761c3f92585b6e69aada56eebb491e8
Muhammad Ahsan <imahsan600@gmail.com>
2025-11-03 00:31:36 +0500

    Fix this env's

--- backend/src/config/clerk.ts
+++ backend/src/config/clerk.ts
@@ -3,6 +3,7 @@ import config from './index'
 
 export const clerkClient = createClerkClient({
 	secretKey: config.CLERK_SECRET_KEY,
+	publishableKey: config.CLERK_PUBLISHABLE_KEY,
 })
 
 export default clerkClient
--- backend/src/lib/openAIModel.ts
+++ backend/src/lib/openAIModel.ts
@@ -3,5 +3,4 @@ import config from '../config'
 
 export const openai = new OpenAI({
 	apiKey: config.OPENAI_API_KEY,
-	project: config.OPENAI_AI_PROJECT_ID,
 })
