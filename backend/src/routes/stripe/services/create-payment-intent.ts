c90fa063f268db3bc173da1074375bef459a8d0b
imahsanvs <imahsan.work@gmail.com>
2026-02-01 01:32:35 +0500

    feat: add a guided setup flow to the admin dashboard and implement Stripe payment intent creation on the backend.

--- backend/src/routes/stripe/services/create-payment-intent.ts
+++ backend/src/routes/stripe/services/create-payment-intent.ts
@@ -1,11 +1,14 @@
 import { FastifyRequest } from 'fastify'
 import stripe from '../../../config/stripe'
-import { getPriceIdAmount } from '../../../utils/subscriptions'
 import { CreatePaymentIntentInput, CreatePaymentIntentOutput } from './types'
 
 export const createPaymentIntent = async (request: FastifyRequest, args: CreatePaymentIntentInput): Promise<CreatePaymentIntentOutput> => {
 	const { businessId, priceId } = args
-	const amount = getPriceIdAmount(priceId)
+
+	const findPrice = await stripe.prices.retrieve(priceId)
+	if (!findPrice) {
+		throw new Error('Price not found')
+	}
 
 	const setupIntent = await stripe.setupIntents.create({
 		payment_method_types: ['card'],
@@ -14,5 +17,6 @@ export const createPaymentIntent = async (request: FastifyRequest, args: CreateP
 			priceId,
 		},
 	})
+	request.log.info(`Payment Intent created successfully`)
 	return { clientSecret: setupIntent.client_secret }
 }
